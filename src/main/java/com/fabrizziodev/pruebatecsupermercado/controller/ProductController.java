cbf0e6057a3a99bca660d929282df79723b89f8e
FabrizzioCarbonell <fabrizzio.ddd@gmail.com>
2025-11-22 13:34:39 -0600

    Feat: GlobalExceptionHandler Added

--- src/main/java/com/fabrizziodev/pruebatecsupermercado/controller/ProductController.java
+++ src/main/java/com/fabrizziodev/pruebatecsupermercado/controller/ProductController.java
@@ -2,8 +2,13 @@ package com.fabrizziodev.pruebatecsupermercado.controller;
 
 import com.fabrizziodev.pruebatecsupermercado.model.dto.ProductDTO;
 import com.fabrizziodev.pruebatecsupermercado.service.IProductService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -11,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@Validated
 public class ProductController {
 
     @Autowired
@@ -22,19 +28,19 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+    public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO){
         ProductDTO product = productService.createProduct(productDTO);
 
         return ResponseEntity.created(URI.create("/api/products/" + product.getProductId())).body(product);
     }
 
     @PutMapping("/{productId}")
-    public ResponseEntity<ProductDTO> updateProducts(@PathVariable Long productId, @RequestBody ProductDTO productDto) {
+    public ResponseEntity<ProductDTO> updateProducts(@PathVariable @Positive(message = "The product ID must be a positive number") Long productId, @Valid @RequestBody ProductDTO productDto) {
         return ResponseEntity.ok(productService.updateProduct(productId, productDto));
     }
 
     @DeleteMapping("/{productId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId){
+    public ResponseEntity<Void> deleteProduct(@PathVariable @Positive(message = "The product ID must be a positive number") Long productId){
         productService.deleteProduct(productId);
 
         return ResponseEntity.noContent().build();
